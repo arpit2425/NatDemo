771b2c7cfe0ad4ce425fd1a6ecd4de141d873179
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-01-15 22:37:30 +0530

    Tours routing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
+app.use(express.json())
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
@@ -11,7 +12,23 @@ app.get('/api/v1/tours', (req, res) => {
     data: tours
   })
 })
-
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      })
+    }
+  )
+})
 app.listen(3000, () => {
   console.log('Server at port 3000')
 })
