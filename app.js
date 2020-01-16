54d9615f5831fe160adf07bd3dcf69c02740ed16
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-01-16 20:25:04 +0530

    Crud Routes

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
-app.use(express.json())
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
+);
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: tours
-  })
-})
+  });
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      message: 'Invalid Tour'
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  }
+});
 app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, req.body)
-  tours.push(newTour)
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -25,10 +41,10 @@ app.post('/api/v1/tours', (req, res) => {
         data: {
           tour: newTour
         }
-      })
+      });
     }
-  )
-})
+  );
+});
 app.listen(3000, () => {
-  console.log('Server at port 3000')
-})
+  console.log('Server at port 3000');
+});
