05b38558715db46a18d448b1adc14fc21e08bf1e
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-01-16 21:59:17 +0530

    Refactor routes

--- app.js
+++ app.js
@@ -5,30 +5,14 @@ app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res) => {
+const getTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: tours
   });
-});
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  if (!tour) {
-    res.status(404).json({
-      message: 'Invalid Tour'
-    });
-  } else {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour
-      }
-    });
-  }
-});
-app.post('/api/v1/tours', (req, res) => {
+};
+const newTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -44,7 +28,30 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      message: 'Invalid Tour'
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  }
+};
+
+app
+  .route('/api/v1/tours')
+  .get(getTours)
+  .put(newTour);
+
+app.route('/api/v1/tours/:id').get(getTour);
 app.listen(3000, () => {
   console.log('Server at port 3000');
 });
