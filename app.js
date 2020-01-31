22b549bad06208d37fbb0e545faecc1c01a5c510
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-01-31 20:55:42 +0530

    Authentication completed

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const globalErrorHandler = require('./controller/errorController');
 const appError = require('./utils/appError');
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   next();
